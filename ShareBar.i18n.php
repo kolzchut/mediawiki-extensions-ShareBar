f7b09c5ce88388463aec041a4f5de27ed8e44e12
Dror S <FreedomFighterSparrow@kolzchut.org.il>
2014-04-13 20:51:15 +0300

    Extension:ShareBar translation to English

--- ShareBar.i18n.php
+++ ShareBar.i18n.php
@@ -8,28 +8,28 @@
 $messages = array();
 
 $messages['en'] = array(
-	'ext-sharebar'            => 'Kol-Zchut ShareBar',
-    'ext-sharebar-desc' => 'ShareBar for for the Kol-Zchut website',
-    'ext-sharebar-loading' => 'Loading...',
-    'ext-sharebar-feedback-form-title' => 'משוב על שימוש באתר',
-    'ext-sharebar-cr-form-title' => 'הצעת שינוי לתוכן האתר',
-    'ext-sharebar-donate-form-title' => 'תרומה לכל-זכות',
-
-    'ext-sharebar-btn-donate' => 'תרומה',
-    'ext-sharebar-btn-changerequest' => 'הצעת שינוי',
-    'ext-sharebar-btn-feedback' => 'משוב',
-
-    'ext-sharebar-share-on' => 'שתפו ב-',
-    'ext-sharebar-send' => 'שלחו כתבה',
-    'ext-sharebar-print' => 'הדפסה',
-    'ext-sharebar-changerequest' => '{{int:ext-sharebar-btn-changerequest}}',
-
-    'ext-sharebar-facebook'	 => '{{int:ext-sharebar-share-on}}',
-	'ext-sharebar-twitter'	     => '{{int:ext-sharebar-share-on}}',
-    'ext-sharebar-gplus'	     => '{{int:ext-sharebar-share-on}}',
-
-    'ext-sharebar-twitter-msg'	 => '$1 ב#$2 - דעו מה מגיע לכם!',
-	'ext-sharebar-facebook-msg'	 => 'מידע על $1 ב$2 - דעו מה מגיע לכם!',
+	'ext-sharebar' => 'Kol-Zchut ShareBar',
+	'ext-sharebar-desc' => 'ShareBar for for the Kol-Zchut website',
+	# forms
+	'ext-sharebar-loading' => 'Loading...',
+	'ext-sharebar-feedback-form-title' => 'Give us feedback',
+	'ext-sharebar-cr-form-title' => 'Submit a change request',
+	'ext-sharebar-donate-form-title' => 'Donate to {{SITENAME}}',
+	# buttons
+	'ext-sharebar-btn-donate' => 'Donate',
+	'ext-sharebar-btn-changerequest' => 'Change request',
+	'ext-sharebar-btn-feedback' => 'Feedback',
+	# bar
+	'ext-sharebar-share-on' => 'Share on ',
+	'ext-sharebar-send' => 'Send article',
+	'ext-sharebar-print' => 'Print',
+	'ext-sharebar-changerequest' => '{{int:ext-sharebar-btn-changerequest}}',
+	'ext-sharebar-facebook'	 => '{{int:ext-sharebar-share-on}}',
+	'ext-sharebar-twitter' => '{{int:ext-sharebar-share-on}}',
+	'ext-sharebar-gplus' => '{{int:ext-sharebar-share-on}}',
+	# sharing texts
+	'ext-sharebar-twitter-msg' => '$1 at #$2 - know your rights!',
+	'ext-sharebar-facebook-msg' => 'Lean about $1 at $2 - know your rights!',
 
 
 );
@@ -38,24 +38,34 @@ $messages['en'] = array(
  * @author Dror Snir
  */
 $messages['qqq'] = array(
-	'wrsharings-desc' => '{{desc}}',
-	'wrsharing-twitter-msg'	=> '$1 is title, $2 SITENAME, $3 for URL',
 );
 
 /** Hebrew (עברית)
  * @author Dror Snir
  */
 $messages['he'] = array(
-	'wrsharings' 		=> 'כלי שיתוף עבור כל-זכות',
-	'wrsharings-desc' 	=> "כלי שיתוף עבור כל-זכות",
-	'wrsharing-facebook'	=> 'שיתוף בפייסבוק',
-	'wrsharing-twitter'	=> 'שיתוף בטוויטר',
-	'wrsharing-twitter-msg'	=> '$1 ב#$2 | $3 (@kolzchut)',
-	'wrsharing-email' 	=> 'שיתוף בדוא"ל',
-	'wrsharing-print' 	=> 'הדפסה',
-	'wrsharing-loading'	     => 'טוען...',
-	'wrsharing-newsletter-btn' => 'הרשמה לעדכונים',
-	'wrsharing-feedback-btn'       => 'משוב על שימוש באתר',
+	'ext-sharebar' => 'סרגל שיתוף עבור כל-זכות',
+	'ext-sharebar-desc' => "כלי שיתוף עבור כל-זכות",
+	# forms
+	'ext-sharebar-loading' => 'בטעינה...',
+	'ext-sharebar-feedback-form-title' => 'משוב על שימוש באתר',
+	'ext-sharebar-cr-form-title' => 'הצעת שינוי לתוכן האתר',
+	'ext-sharebar-donate-form-title' => 'תרומה לכל-זכות',
+	# buttons
+	'ext-sharebar-btn-donate' => 'תרומה',
+	'ext-sharebar-btn-changerequest' => 'הצעת שינוי',
+	'ext-sharebar-btn-feedback' => 'משוב',
+	# bar
+	'ext-sharebar-share-on' => 'שתפו ב-',
+	'ext-sharebar-send' => 'שלחו כתבה',
+	'ext-sharebar-print' => 'הדפסה',
+	'ext-sharebar-changerequest' => '{{int:ext-sharebar-btn-changerequest}}',
+	'ext-sharebar-facebook'	 => '{{int:ext-sharebar-share-on}}',
+	'ext-sharebar-twitter' => '{{int:ext-sharebar-share-on}}',
+	'ext-sharebar-gplus' => '{{int:ext-sharebar-share-on}}',
+	# sharing texts
+	'ext-sharebar-twitter-msg' => '$1 ב#$2 - דעו מה מגיע לכם!',
+	'ext-sharebar-facebook-msg' => 'מידע על $1 ב$2 - דעו מה מגיע לכם!',
 );
 
 /**
