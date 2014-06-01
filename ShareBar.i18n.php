53b883a8506999968b01d509561cae828e235920
Dror S <FreedomFighterSparrow@kolzchut.org.il>
2014-06-01 18:05:43 +0300

    Add Arabic translation to Extension:ShareBar

--- ShareBar.i18n.php
+++ ShareBar.i18n.php
@@ -7,36 +7,6 @@
 
 $messages = array();
 
-/**
-/* Arabic (العربية)
- * @author Jalal Hassan
- * @author Suheir Daksa-Halabi
- */
-$messages['ar'] = array(
-	'ext-sharebar' => 'Kol-Zchut ShareBar',	// Name of this extension, only shows in credits
-	'ext-sharebar-desc' => 'ShareBar for for the Kol-Zchut website', // Description of this extension, only shows in credits
-	# forms
-	'ext-sharebar-loading' => 'تحميل...',	// Text to show while loading sharing forms
-	'ext-sharebar-feedback-form-title' => 'Give us feedback',	// Title for the feedback form
-	'ext-sharebar-cr-form-title' => 'Submit a change proposal',	// Tooltip for change proposal button
-	'ext-sharebar-donate-form-title' => 'Donate to {{SITENAME}}',	// Title for the donation form
-	# buttons
-	'ext-sharebar-btn-donate' => 'Donate',	// Donation button
-	'ext-sharebar-btn-changerequest' => 'اقتراح تغيير',	// Button
-	'ext-sharebar-btn-feedback' => 'تقييم الموقع',	// Button which loads a feedback form
-	# bar
-	// The following is a prefix to sharing buttons, which show the logo for each service, so it looks like this:
-	// "Share on [facebook logo]"
-	'ext-sharebar-share-on' => 'Share on&nbsp;&nbsp;',
-	'ext-sharebar-send' => 'Send article',	// Share an article with a friend, through email at the moment
-	'ext-sharebar-print' => 'للطباعة',	// Print button
-
-	# sharing texts
-	'ext-sharebar-twitter-msg' => '$1 at #$2 - know your rights!',	// Template text for sharing on Twitter
-	'ext-sharebar-facebook-msg' => 'Learn about $1 at $2 - know your rights!', // Template text for sharing on Facebook
-);
-
-
 $messages['en'] = array(
 	'ext-sharebar' => 'Kol-Zchut ShareBar',	// Name of this extension, only shows in credits
 	'ext-sharebar-desc' => 'ShareBar for for the Kol-Zchut website', // Description of this extension, only shows in credits
@@ -99,3 +69,37 @@ $messages['he'] = array(
 	'ext-sharebar-twitter-msg' => '$1 ב#$2 - דעו מה מגיע לכם!',
 	'ext-sharebar-facebook-msg' => 'מידע על $1 ב$2 - דעו מה מגיע לכם!',
 );
+
+/**
+/* Arabic (العربية)
+ * @author Ahlam Rahal
+ * @author Jalal Hassan
+ * @author Suheir Daksa-Halabi
+ */
+$messages['ar'] = array(
+	'ext-sharebar' => 'لوحة مشاركة كل الحق',	// Name of this extension, only shows in credits
+	'ext-sharebar-desc' => 'أدوات مشاركة كل الحق',	// Description of this extension, only shows in credits
+	# forms
+	'ext-sharebar-loading' => 'تحميل...',	// Text to show while loading sharing forms
+	'ext-sharebar-feedback-form-title' => ' رد فعل حول استخدام الموقع',	// Title for the feedback form
+	'ext-sharebar-cr-form-title' => 'اكتب اقتراح تغيير',	// Tooltip for change proposal button
+	'ext-sharebar-donate-form-title' => 'تبرع لـ {{SITENAME}}',	// Title for the donation form
+	# buttons
+	'ext-sharebar-btn-donate' => 'تبرع',	// Donation button
+	'ext-sharebar-btn-changerequest' => 'اقتراح تغيير',	// Button
+	'ext-sharebar-btn-feedback' => 'رد فعل',	// Button which loads a feedback form
+	# bar
+	// The following is a prefix to sharing buttons, which show the logo for each service, so it looks like this:
+	// "Share on [facebook logo]"
+	'ext-sharebar-share-on' => 'شارك&nbsp;',
+	'ext-sharebar-send' => 'أرسل المقال',	// Share an article with a friend, through email at the moment
+	'ext-sharebar-print' => 'طباعة',	// Print button
+	'ext-sharebar-changerequest' => '{{int:ext-sharebar-btn-changerequest}}',	// Do not translate
+	'ext-sharebar-facebook'     => '{{int:ext-sharebar-share-on}}', // Do not translate
+	'ext-sharebar-twitter' => '{{int:ext-sharebar-share-on}}', // Do not translate
+	'ext-sharebar-gplus' => '{{int:ext-sharebar-share-on}}', // Do not translate
+	# sharing texts
+	'ext-sharebar-twitter-msg' => '$1 حتى #$2 – اعرف حقوقك!',    // Template text for sharing on Twitter
+	'ext-sharebar-facebook-msg' => ' تعلم حول $1 حتى $2 – اعرف حقوقك!', // Template text for sharing on Facebook
+);
+
